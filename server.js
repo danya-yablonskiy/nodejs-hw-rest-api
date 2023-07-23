fcd51f0891c1614c621abc0a7394874543a26d91
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-07-23 03:51:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Danya:kXcadnBX9t0MH3rV@cluster0.xkunj76.mongodb.net/db-contacts";
 
+// mongoose.set('strictQuery', true)
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
