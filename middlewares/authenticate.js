28a1106d840a65b2491e09dd965ccafd96da0324
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-07-30 18:36:33 +0300

    Create authenticate.js

---
+++ middlewares/authenticate.js
@@ -0,0 +1,30 @@
+const AppError = require("../utils/AppError");
+
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../controllers/auth");
+const User = require("../models/userModel");
+
+const authecticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer") {
+    next(AppError(401, "Unauthorized"));
+  }
+
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+
+    if (!user || !user.token || user.token !== token) {
+      next(AppError(401, "Unauthorized"));
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(AppError(401, "Unauthorized"));
+  }
+};
+module.exports = authecticate;
