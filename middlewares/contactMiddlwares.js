8f688886d1c96c6447d34d1688bc1b8ed4beeddb
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-08-04 17:15:42 +0300

    Update contactMiddlwares.js

--- middlewares/contactMiddlwares.js
+++ middlewares/contactMiddlwares.js
@@ -1,20 +1,11 @@
-// const { Types } = require("mongoose");
 const { isValidObjectId } = require("mongoose");
 const AppError = require("../utils/AppError");
 
 const checkContactId = async (req, res, next) => {
-  //   const id = req.params.contactId;
-  //   const isValidId = Types.ObjectId.isValid(id);
-    
-  // ?????????????????????? Чому тут false ???????????????????????
-  //   console.log(isValidId);
-    
-  //   if (!isValidId) throw AppError(404, "Contact does not exists");
-  //   next();
-    if (!isValidObjectId(req.params.contactId)) {
-      next(AppError(400, "Invalid id"));
-    }
-    next();
+  if (!isValidObjectId(req.params.contactId)) {
+    next(AppError(400, "Invalid id"));
+  }
+  next();
 };
 
 module.exports = checkContactId;
