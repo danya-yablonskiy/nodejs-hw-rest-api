f23c630ba8d72b9caf79c778aea12e5284e2c797
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-08-04 17:15:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-// const dotenv = require("dotenv");
+
 require("dotenv").config();
 const contactsRouter = require("./routes/api/contacts");
 
 const authRouter = require("./routes/api/auth");
 
-// dotenv.config({ path: "./.env" });
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
